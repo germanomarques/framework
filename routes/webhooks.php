f32a6c21c977018c402fdfeaec50d4a66f0ea496
Vladimir Yuldashev <misterio92@gmail.com>
2018-08-28 00:13:56 +0300

    wip

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{channel}/{secret?}', 'WebhookController@store')->name('fondbot.webhook');
-Route::post('/{channel}/{secret?}', 'WebhookController@store')->name('fondbot.webhook');
+Route::get('/{channel}/{secret?}', 'WebhookController@store')->name('webhook');
+Route::post('/{channel}/{secret?}', 'WebhookController@store')->name('webhook');
